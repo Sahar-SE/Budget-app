75c68278896fa6525fc0d18a5fa493788cd86c1f
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-10-21 18:43:19 +0430

    Add model test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Write validation tests for user model' do
+    it 'is not valid without a name' do
+      first_user = User.create(name: nil, email: 'user1@email.com', password: 'password',
+                               password_confirmation: 'password')
+      expect(first_user).to_not be_valid
+    end
+
+    it 'is not valid without an email' do
+      first_user = User.create(name: 'Mavericks', email: nil, password: 'password',
+                               password_confirmation: 'password')
+      expect(first_user).to_not be_valid
+    end
+  end
 end
