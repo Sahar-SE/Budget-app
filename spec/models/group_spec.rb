7b899b7c0e9c3d3d3dae7289e14b8a8ab15d7708
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-10-21 18:42:45 +0430

    Add model test for group

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Write validation tests for group model' do
+    it 'is not valid without a name' do
+      first_group = Group.create(name: nil, icon: 'icon')
+      expect(first_group).to_not be_valid
+    end
+
+    it 'is not valid without an icon' do
+      first_group = Group.create(name: 'name', icon: nil)
+      expect(first_group).to_not be_valid
+    end
+  end
 end
