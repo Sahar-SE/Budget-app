f389b1d96ffb77d1c73fbccb0fc8909794552ab0
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-10-21 16:34:40 +0430

    Add budget model

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,4 +2,7 @@ class Budget < ApplicationRecord
   belongs_to :user
   has_many :group_budgets, dependent: :destroy
   has_many :groups, through: :group_budgets
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
